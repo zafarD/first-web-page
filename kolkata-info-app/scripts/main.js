d7e93ffbe8c1b8ed5caad37d1d313ccf0c2c8f87
zafarD <zafarahasan1703@gmail.com>
2023-04-13 00:33:06 +0530

    Restructuring My project web page

---
+++ kolkata-info-app/scripts/main.js
@@ -0,0 +1,32 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent =  "Hello Zafar!";
+
+// document.querySelector("html").addEventListener("click", 
+// function() { // () => {code block}; also a function
+//     alert("Ouch! Stop poking me!");
+// });
+
+// let myBotton = document.querySelector("button");
+// let myHeading = document.querySelector("h1");
+
+// function setUserName() {
+//     const myName = prompt("Please enter your name -> ");
+//     if(!myName) {
+//         setUserName();
+//     }
+//     else {
+//         localStorage.setItem("name", myName);
+//         myHeading.textContent = `Sunset is cool, ${myName}`;
+//     }
+// }
+
+// if (!localStorage.getItem("name")) {
+//     setUserName();
+// } else {
+//     const storedName = localStorage.getItem("name");
+//     myHeading.textContent = `Sunset is cool, ${storedName}`;
+// }
+
+// myButton.onclick = () => {
+//     setUserName();
+// };
