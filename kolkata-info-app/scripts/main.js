fca51933dd21a3230d83abad2f05660adb12ff87
zafarD <zafarahasan1703@gmail.com>
2023-09-08 22:22:52 +0530

    08.09.04

--- kolkata-info-app/scripts/main.js
+++ kolkata-info-app/scripts/main.js
@@ -1,32 +1,33 @@
+    //Basic
 // const myHeading = document.querySelector("h1");
 // myHeading.textContent =  "Hello Zafar!";
 
 // document.querySelector("html").addEventListener("click", 
-// function() { // () => {code block}; also a function
+// function() { // () => {code block}; called a arrow function
 //     alert("Ouch! Stop poking me!");
 // });
 
-// let myBotton = document.querySelector("button");
-// let myHeading = document.querySelector("h1");
+let myBotton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
 
-// function setUserName() {
-//     const myName = prompt("Please enter your name -> ");
-//     if(!myName) {
-//         setUserName();
-//     }
-//     else {
-//         localStorage.setItem("name", myName);
-//         myHeading.textContent = `Sunset is cool, ${myName}`;
-//     }
-// }
+function setUserName() {
+    const myName = prompt("Please enter your name: ");
+    if(!myName) {
+        setUserName();
+    }
+    else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Welcome to Kolkata The city of joy ${myName} !!!`;
+    }
+}
 
-// if (!localStorage.getItem("name")) {
-//     setUserName();
-// } else {
-//     const storedName = localStorage.getItem("name");
-//     myHeading.textContent = `Sunset is cool, ${storedName}`;
-// }
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome to Kolkata, The city of joy ${storedName} !!!`;
+}
 
-// myButton.onclick = () => {
-//     setUserName();
-// };
+myButton.onclick = () => {
+    setUserName();
+};
